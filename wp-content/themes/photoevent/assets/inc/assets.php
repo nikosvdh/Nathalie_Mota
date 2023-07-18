36b5533c676568c629beee8633c489cb39bf31a3
nikosvdhgit config --global user.name nikosvdhgit config --global user.name nikosvdh <nikolavdh@gmail.com>
2023-07-19 00:28:04 +0200

    add fonts + plugins WPFORMS & ALLIN1MIGRATION

--- wp-content/themes/photoevent/assets/inc/assets.php
+++ wp-content/themes/photoevent/assets/inc/assets.php
@@ -5,6 +5,6 @@ function photoevent_enqueue_styles()
 {
     wp_enqueue_style("style_header", get_template_directory_uri() . '/assets/css/header.css');
     wp_enqueue_style("style_footer", get_template_directory_uri() . '/assets/css/footer.css');
-
+    wp_enqueue_style("style_footer", get_template_directory_uri() . '/assets/css/typo.css');
 }
 add_action('wp_enqueue_scripts', 'photoevent_enqueue_styles');
--- wp-content/themes/photoevent/footer.php
+++ wp-content/themes/photoevent/footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="footer">
 
 
     <?php wp_nav_menu([
