982133243b326d2bca39098c9adeb1c8f3b02dac
nikosvdhgit config --global user.name nikosvdhgit config --global user.name nikosvdh <nikolavdh@gmail.com>
2023-07-27 00:32:19 +0200

    add arrows and slider single

--- wp-content/themes/photoevent/single-photo.php
+++ wp-content/themes/photoevent/single-photo.php
@@ -7,7 +7,7 @@
 <?php get_header(); ?>
 
 <?php
-/* Start the Loop  - Section photo + info */
+/* Start the Loop - PHOTO */
 while ( have_posts() ) :
 	the_post();
 ?>
@@ -43,5 +43,72 @@ while ( have_posts() ) :
 <?php
 endwhile; // End of the loop.
 ?>
+
+<!-- CTA CONTACT -->
+
+<div class="is-interested">
+    <div class="is-interested-text-button">
+        <p class="is-interested-text">Cette photo vous intéresse ?</p>
+        <button class="button">Contact</button>
+    </div>
+
+    <div class="is-interested-slider">
+        <?php
+		$previousPhoto = get_previous_post();
+		$nextPhoto = get_next_post();
+		?>
+
+        <div class="is-interested-arrows">
+            <?php if (!empty($previousPhoto)) {
+				$previousThumbnail = get_the_post_thumbnail_url($previousPhoto->ID);
+				$previousLink = get_permalink($previousPhoto); ?>
+            <a href="<?php echo $previousLink; ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Arrow_left.png"
+                    alt="Flèche vers la gauche" />
+            </a>
+            <?php } else { ?>
+            <img style="opacity:0; cursor: auto;"
+                src="<?php echo get_template_directory_uri(); ?>/assets/images/Arrow_left.png"
+                alt="Flèche vers la gauche" />
+            <?php }
+			if (!empty($nextPhoto)) {
+				$nextThumbnail = get_the_post_thumbnail_url($nextPhoto->ID);
+				$nextLink = get_permalink($nextPhoto); ?>
+            <a href="<?php echo $nextLink; ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Arrow_right.png"
+                    alt="Flèche vers la droite" />
+            </a>
+            <?php } else { ?>
+            <img style="opacity:0; cursor: auto;"
+                src="<?php echo get_template_directory_uri(); ?>/assets/images/Arrow_right.png"
+                alt="Flèche vers la droite" />
+            <?php } ?>
+        </div>
+        <div class="img-container">
+            <div>
+                <?php
+				if (isset($previousThumbnail) && !empty($previousThumbnail)) {
+					// Afficher l'image suivante
+					echo '<img class="previous-img" src="' . $previousThumbnail . '" alt="affichage de la photo précédente" />';
+				} else {
+					// Afficher un message d'erreur ou une image par défaut
+					echo '<p></p>';
+				}
+				?>
+            </div>
+            <div>
+                <?php
+				if (isset($nextThumbnail) && !empty($nextThumbnail)) {
+					// Afficher l'image suivante
+					echo '<img class= "next-img" src="' . $nextThumbnail . '" alt="affichage de la photo suivante" />';
+				} else {
+					// Afficher un message d'erreur ou une image par défaut
+					echo '<p></p>';
+				}
+				?> </div>
+        </div>
+    </div>
+
+</div>
 
 <?php get_footer(); ?>
